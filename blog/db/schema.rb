46883711644861379a4e350042a34ce99bdb8b46
magk <bluevnd@gmail.com>
2018-08-18 16:45:26 +0700

    create table in db

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180818094028) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "published_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
